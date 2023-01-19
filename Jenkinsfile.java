405eda19d8ae342432f9fd32491466251ae64838
Iresh M Dissanayaka <socialexpz1@gmail.com>
2023-01-19 22:48:17 +0530

    update Jenkinsfile.{ace,java} to separate repositories

--- Jenkinsfile.java
+++ Jenkinsfile.java
@@ -1,14 +1,14 @@
-def maven_artifact_repositories_json
+def maven_java_artifact_repositories_json
 
 node {
-    configFileProvider( [configFile(fileId: 'maven-artifact-repositories', variable: 'maven_artifact_repositories')]) {
+    configFileProvider( [configFile(fileId: 'maven-java-artifact-repositories', variable: 'maven_java_artifact_repositories')]) {
         script {
-            maven_artifact_repositories_json = readFile maven_artifact_repositories
+            maven_java_artifact_repositories_json = readFile maven_java_artifact_repositories
         }
     }
 }
 
 javarelease {
-        artifactRepositoriesJSONDefaultValue = maven_artifact_repositories_json 
+        artifactRepositoriesJSONDefaultValue = maven_java_artifact_repositories_json 
         gitRepositoryUrlDefaultValue = "${git_repo_url}"
 }
